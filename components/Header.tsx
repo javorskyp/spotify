d72d4f2b2572830c62c299ebb519df9cc083a7cc
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-08-07 14:27:34 +0200

    Auth Modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import {twMerge } from 'tailwind-merge'
 import { RxCaretLeft, RxCaretRight} from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import useAuthModal from '@/hooks/useAuthModal';
 import Button from './Button'
 
 interface HeaderProps {
@@ -16,6 +17,7 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter ();
     const handleLogout = () => {
 
@@ -45,13 +47,13 @@ const Header: React.FC<HeaderProps> = ({
                 <div className='flex justify-between items-center gap-x-4'>
                     <>
                         <div>
-                            <Button onClick={() => {}}
+                            <Button onClick={authModal.onOpen}
                                 className='bg-transparent text-neutral-300 font-medium'>
                                 Sign up
                             </Button>
                         </div>
                         <div>
-                            <Button onClick={() => {}}
+                            <Button onClick={authModal.onOpen}
                                 className='bg-white px-6 py-2'>
                                 Log in
                             </Button>
