357eccb610b29d80dd10df538bc639ab2ca9b201
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-08-21 18:33:36 +0200

    PlayerFunctionality, Slider

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import usePlayer from "@/hooks/usePlayer";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import useGetSongById from "@/hooks/useGetSongById";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
@@ -10,23 +11,13 @@ const Player = () => {
 
   const songUrl = useLoadSongUrl(song!);
 
-  if (!song || !songUrl || !player.activeId) {
+   if (!song || !songUrl || !player.activeId) {
     return null;
   }
 
   return (
-    <div 
-      className="
-        fixed 
-        bottom-0 
-        bg-black 
-        w-full 
-        py-2 
-        h-[80px] 
-        px-4
-      "
-    >
-      Player
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+        <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 }
