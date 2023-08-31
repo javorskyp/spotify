221f6d7629ea169d5d15134df4991c81f615d903
Pawel Jaworski Software <jpsoftware@macbook-pro-pawel.home>
2023-08-31 23:58:58 +0200

    refactor

--- components/Box.tsx
+++ components/Box.tsx
@@ -10,16 +10,7 @@ const Box: React.FC<BoxProps> = ({
   className
  }) => {
   return ( 
-    <div 
-      className={twMerge(
-        `
-        bg-neutral-900 
-        rounded-lg 
-        h-fit 
-        w-full
-        `, 
-        className
-      )}>
+    <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>
       {children}
     </div>
   );
